bbda382c857170c6fdfc2b901ba9661895da8bc4
Nesat-Nayem <nesatnayem042@gmail.com>
2021-10-11 15:08:29 +0600

    ema jon site

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBi58mRGSAd3fr3LrStLfee-8f3f0g6Z8c",
+    authDomain: "simple-firebase-authenti-ed0cf.firebaseapp.com",
+    projectId: "simple-firebase-authenti-ed0cf",
+    storageBucket: "simple-firebase-authenti-ed0cf.appspot.com",
+    messagingSenderId: "350405292669",
+    appId: "1:350405292669:web:18c38b81750380dee2f714"
+};
+
+export default firebaseConfig;
